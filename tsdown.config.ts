cb91a081161f63e0591992481d4ec39a9e6f82af
Mike Thorpe <mike.thorpe@shortcut.com>
2025-11-13 13:19:48 -0800

    Cleanup and removing SSE server stuff, renaming to server-http

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
   entry: {
     index: 'index.ts',
-    'server-sse': 'src/server-sse.ts',
-    'server-shttpt': 'src/server-shttpt.ts',
+    'server-http': 'src/server-http.ts',
   },
   outDir: 'dist',
   format: 'esm',
