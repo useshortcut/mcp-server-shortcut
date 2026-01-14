1e049b8aa9e31b4eaca26cbfc2f69589ebd9300c
Mike Thorpe <mike.thorpe@shortcut.com>
2026-01-14 15:02:21 -0800

    Add awslogger

--- codedeploy/scripts/application_start.sh
+++ codedeploy/scripts/application_start.sh
@@ -4,4 +4,8 @@ echo "Application Start -----"
 docker run \
  --name shortcut-mcp \
  --rm -d -p 9292:9292 \
+ --log-driver=awslogs \
+ --log-opt awslogs-region=us-east-1 \
+ --log-opt awslogs-group="shortcut/${DEPLOYMENT_GROUP}/${APPLICATION_NAME}" \
+ --log-opt awslogs-create-stream=true \
  048776562964.dkr.ecr.us-east-1.amazonaws.com/shortcut/mcp-server
