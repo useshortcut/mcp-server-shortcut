9fc0f9aac419893c413b45d03f13f492defe9dec
Mike Thorpe <mike.thorpe@shortcut.com>
2026-01-14 15:24:14 -0800

    Used wrong variable.

--- codedeploy/scripts/application_start.sh
+++ codedeploy/scripts/application_start.sh
@@ -6,5 +6,5 @@ docker run \
  --rm -d -p 9292:9292 \
  --log-driver=awslogs \
  --log-opt awslogs-region=us-east-1 \
- --log-opt awslogs-group="/shortcut/${DEPLOYMENT_GROUP}/${APPLICATION_NAME}" \
+ --log-opt awslogs-group="/shortcut/${DEPLOYMENT_GROUP_NAME}/${APPLICATION_NAME}" \
  048776562964.dkr.ecr.us-east-1.amazonaws.com/shortcut/mcp-server
