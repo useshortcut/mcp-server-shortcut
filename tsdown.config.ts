9479990150b5648d15a053251454a0b17d1927a2
Mike Thorpe <mike.thorpe@shortcut.com>
2026-02-04 14:00:22 -0800

    Update build step to include local server

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
   entry: {
     index: 'index.ts',
+    server: 'src/server.ts',
     'server-http': 'src/server-http.ts',
   },
   outDir: 'dist',
